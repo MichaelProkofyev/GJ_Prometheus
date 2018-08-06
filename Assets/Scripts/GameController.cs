9b41dbf1642fe77782fc428b05b0c34bd64d7cfe
Michael Prokofyev <mikeprokofyev@gmail.com>
2018-08-06 09:56:26 +0300

    end of the jam

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,20 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 enum GameState
 {
     QUESTION,
     COMMAND,
     REVEAL,
     DECISION,
-    
+    CAPTURED,
 }
 
 public class GameController : MonoBehaviour {
 
+    public GameObject textBackground;
+    public Renderer humanRenderer = null;
     public TMPro.TextMeshProUGUI mainText;
-    public ParticleSystem particles;
+    public TMPro.TextMeshProUGUI statsText;
+    public GameObject capturedPanel;
+    public ParticleSystem particlesFire;
+    public ParticleSystem particlesIce;
+    public GameObject endNoise;
+    public Button yesButton;
+    public Button noButton;
+    public int humanIndex = 0;
+    public int falseAccusations = 0;
+
+    public AudioSource audioPlayer;
+    public AudioClip endThemeClip;
+
+    public bool isPrometheus = false;
+    public Color matchQuestionColor;
 
     GameState state = GameState.QUESTION;
 
@@ -24,35 +40,77 @@ public class GameController : MonoBehaviour {
         set 
         {
             humanRenderer.material.SetFloat("_NoiseLerp", 0);
-            particles.enableEmission = false;
+            particlesFire.enableEmission = false;
+            particlesIce.enableEmission = false;
+            yesButton.gameObject.SetActive(false);
+            noButton.gameObject.SetActive(false);
+            capturedPanel.SetActive(false);
+            textBackground.gameObject.SetActive(false);
+            endNoise.SetActive(false);
+            audioPlayer.volume = 0;
             switch (value)
             {
                 case GameState.QUESTION:
-                    mainText.text = "Got a match?";
+                    textBackground.gameObject.SetActive(true);
+                    mainText.text = "Another one!!!";
                     break;
                 case GameState.COMMAND:
-                    mainText.text = "SHED THY CLOTHES!!!";
+                    textBackground.gameObject.SetActive(true);
+                    //Generate new body
+                    if (humanIndex == 0)
+                    {
+                        isPrometheus = false;
+                    }
+                    else
+                    {
+                        isPrometheus = Random.value > 0.5f;
+                    }
+                    mainText.text = "Got a light!?\nSHED THY CLOTHES!!!";
+                    statsText.text = "FALSE ACCUSATIONS: " + falseAccusations;
+                    
                     break;
                 case GameState.REVEAL:
+                    audioPlayer.volume = 1;
+                    audioPlayer.Play();
+                    textBackground.gameObject.SetActive(false);
                     humanRenderer.material.SetFloat("_NoiseLerp", 1);
-                    particles.enableEmission = true;
+                    particlesFire.enableEmission = isPrometheus;
+                    particlesIce.enableEmission = isPrometheus == false;
                     mainText.text = "...";
+                    StartCoroutine("RevealRoutine");
                     break;
                 case GameState.DECISION:
-
-                    mainText.text = "Does he posess a match?";
+                    textBackground.gameObject.SetActive(true);
+                    yesButton.gameObject.SetActive(true);
+                    noButton.gameObject.SetActive(true);
+                    string hexParamColor = ColorUtility.ToHtmlStringRGB(matchQuestionColor);
+                    mainText.text = string.Format("Does this one carry a <color=#{0}>light</color>?", hexParamColor);
+                    break;
+                case GameState.CAPTURED:
+                    audioPlayer.volume = 1;
+                    audioPlayer.clip = endThemeClip;
+                    audioPlayer.Play();
+                    textBackground.gameObject.SetActive(true);
+                    mainText.gameObject.SetActive(false);
+                    capturedPanel.SetActive(true);
+                    endNoise.SetActive(true);
+                    humanRenderer.gameObject.SetActive(false);
                     break;
             }
             state = value;
         }
 
     }
-    
-    [SerializeField] private Renderer humanRenderer = null;
+   
 
+    IEnumerator RevealRoutine()
+    {
+        yield return new WaitForSeconds(5f);
+        State = GameState.DECISION;
+    }
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         State = GameState.QUESTION;
 	}
 	
@@ -62,13 +120,13 @@ public class GameController : MonoBehaviour {
         {
 
             case GameState.QUESTION:
-                if (Input.GetKeyDown(KeyCode.Space))
+                if (Input.GetKeyDown(KeyCode.Mouse0))
                 {
                     State = GameState.COMMAND;
                 }
                 break;
             case GameState.COMMAND:
-                if (Input.GetKeyDown(KeyCode.Space))
+                if (Input.GetKeyDown(KeyCode.Mouse0))
                 {
                     State = GameState.REVEAL;
                 }
@@ -76,15 +134,39 @@ public class GameController : MonoBehaviour {
             case GameState.REVEAL:
 
                 //TODO: Remove manual control, add delay & transition to decision
-                if (Input.GetKeyDown(KeyCode.Space))
+                if (Input.GetKeyDown(KeyCode.Mouse0))
                 {
+                    StopAllCoroutines();
                     State = GameState.DECISION;
                 }
                 break;
             case GameState.DECISION:
                 break;
         }
-
-
 	}
+
+    public void QuessedYes()
+    {
+
+        if (isPrometheus)
+        {
+            State = GameState.CAPTURED;
+        }
+        else
+        {
+            falseAccusations++;
+            humanIndex++;
+            State = GameState.QUESTION;
+        }
+    }
+
+    public void QuessedNo()
+    {
+        if (isPrometheus)
+        {
+            falseAccusations++;
+        }
+        humanIndex++;
+        State = GameState.QUESTION;
+    }
 }
---
+++ Assets/Shaders/EndNoise.shader
@@ -0,0 +1,78 @@
+﻿Shader "Custom/EndNoise"
+{
+	Properties
+	{
+        _NoiseScale ("Noise scale", Range (0,1000)) = 0
+		_NoiseTexture ("Noise Texture", 2D) = "white" {}
+
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+            #include "SimplexNoise3D.hlsl"
+            //#include "SimplexNoise2D.hlsl"
+            static const float PI = 3.14159265f;
+
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+            float _NoiseScale;
+			sampler2D _NoiseTexture;
+
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+                //Getting the noise
+                float2 uv = i.uv * _NoiseScale; //+ float2(0.2, 0.5) * _Time.y;
+                float noise_value = 0.5;
+                float s = 1.0;
+                float w = 0.25;
+
+                for (int iter = 0; iter < 6; iter++)
+                {
+                    float3 coord = float3(uv * s, _Time.z);
+                    float3 period = float3(s, s, 1.0) * 2.0;
+                    noise_value += snoise(coord ) * w;
+
+                    s *= 2.0;
+                    w *= 0.5;
+                }
+                //NON LINEAR RANDOM COLOR SAMPLING
+                noise_value = saturate(noise_value);
+                fixed4 noise_color = tex2D(_NoiseTexture, float2(0.5, noise_value));
+
+				return noise_color;
+			}
+			ENDCG
+		}
+	}
+}
